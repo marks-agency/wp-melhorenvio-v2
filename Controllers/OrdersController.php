db117625a9988af143e26738ea60ee6f26ce420f
claudionhangapc <claudionhangapc@gmail.com>
2023-03-27 15:51:34 -0300

    send email

--- Controllers/OrdersController.php
+++ Controllers/OrdersController.php
@@ -206,6 +206,9 @@ class OrdersController {
 
 		$labelResult = ( new OrderService() )->createLabel( $postId );
 
+		$oi_mark_rastreio_cod_melhor_envio = (new TrackingService())->getTrackingOrder($postId);
+		do_action('oi_mark_melhor_envio_send_email_by', $postId,$oi_mark_rastreio_cod_melhor_envio);
+
 		return wp_send_json(
 			array(
 				'success' => true,
